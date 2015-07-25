baa33a7a6d1b5b9ae8d779197fb592fea05c0808
Garrett Singer <gesinger@gmail.com>
2015-07-24 18:18:41 -0700

    Add out file command line positional arg

--- usdapy.py
+++ usdapy.py
@@ -1,5 +1,6 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='Parser for the USDA SR DB')
-parser.add_argument('srdir', help='directory of SR DB files')
+parser.add_argument('sr_dir', help='directory of SR DB files')
+parser.add_argument('out_file', help='output file name')
 args = parser.parse_args()
